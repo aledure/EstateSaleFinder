04f2b5f3ef105a822bfbbffa0d0ea4dca81311a6
Brooklyn Sides <brooklynjsides@gmail.com>
2024-04-28 20:37:08 -0500

    auth register and login

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,41 @@
-  require("dotenv").config();
+require("dotenv").config();
 require("express-async-errors");
-const cors = require("cors");
-
 
 const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
+// routers
+const authRouter = require("./routes/auth");
 
-app.use(express.json)
-app.use(cors())
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>MarketSpace</h1>");
+});
 
 // routes
-// app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/auth", authRouter);
 
-
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, () =>
-            console.log(`Server is listening on port ${port}...`)
-        );
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+start();
